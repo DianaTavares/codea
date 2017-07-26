3c183961198dd9d68f5d8b0100e6711b080164f3
DianaTavares <dianab_lopez@hotmail.com>
2017-07-25 20:39:45 -0500

    rayuela, ya recorre el tablero para ambos jugadores

--- 4-DesarrolloWeb/Front-End1/rayuela/public/js/application.js
+++ 4-DesarrolloWeb/Front-End1/rayuela/public/js/application.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
 
 	//Se activa una funcion cuando se hace click al boton con id #start_btn
 	$( "#start_btn" ).click(function(){ 
-		lanzarMoneda();
+		//inicia el movimiento en las casillas para ambos jugadores.
+		lanzarMoneda("#Player1");
+		lanzarMoneda("#Player2");
 	});
 
-	//------------------------>Funcion lanzar moneda: quita y agrega classes a las celdas en la tabla.
-	function lanzarMoneda() {
-		var $current_player = $("#Player1").find(".active").removeClass("active");
+	//------------------------>Funcion lanzar moneda: quita y agrega la clase "active" a las celdas en la tabla, tiene un setTimer para hacer el movimienot lento y observable al usuario.
+	function lanzarMoneda(player) {
+		var $current_player = $(player).find(".active").removeClass("active");
 		$current_player.next().addClass("active");
 		if (29>= $($current_player).index()){
 			setTimeout(function(){
-				lanzarMoneda();
+				lanzarMoneda(player);
 			},50);
 		};
 	};
